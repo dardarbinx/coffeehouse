b2d2906c73eabcdc8c3e8ed3f688bae7694f8573
Darleen Jansen <darleen_jansen@hotmail.com>
2017-12-24 09:02:30 +0100

    updated package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TopbarComponent } from './topbar/topbar.component';
