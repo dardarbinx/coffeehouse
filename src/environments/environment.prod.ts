57971ce5f1d02c8adc6775ae569ab80c7cca3809
Darleen Jansen <darleen_jansen@hotmail.com>
2017-12-26 21:45:06 +0100

    firebase config in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAAEWWoAlWBYl8VwsaXQR_g9JCf0jcuRYs',
+    authDomain: 'coffeehouse-71001.firebaseapp.com',
+    databaseURL: 'https://coffeehouse-71001.firebaseio.com',
+    projectId: 'coffeehouse-71001',
+    storageBucket: 'coffeehouse-71001.appspot.com',
+    messagingSenderId: '573611062532'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAAEWWoAlWBYl8VwsaXQR_g9JCf0jcuRYs",
-    authDomain: "coffeehouse-71001.firebaseapp.com",
-    databaseURL: "https://coffeehouse-71001.firebaseio.com",
-    projectId: "coffeehouse-71001",
-    storageBucket: "coffeehouse-71001.appspot.com",
-    messagingSenderId: "573611062532"
+    apiKey: 'AIzaSyAAEWWoAlWBYl8VwsaXQR_g9JCf0jcuRYs',
+    authDomain: 'coffeehouse-71001.firebaseapp.com',
+    databaseURL: 'https://coffeehouse-71001.firebaseio.com',
+    projectId: 'coffeehouse-71001',
+    storageBucket: 'coffeehouse-71001.appspot.com',
+    messagingSenderId: '573611062532'
   }
 };
