aa089ef39d02e0937e8c02765581c0048d48f116
Darleen Jansen <darleen_jansen@hotmail.com>
2017-12-31 13:06:26 +0100

    validation for date field

--- coffeehouse-app/src/app/_shared/validators/date-validator.ts
+++ coffeehouse-app/src/app/_shared/validators/date-validator.ts
@@ -3,19 +3,19 @@ import * as moment from 'moment';
 import { Moment } from 'moment';
 
 export function dateValidator(): ValidatorFn {
-   return (control: AbstractControl): {
-      [key: string]: any
-    } => {
-      const value = control.value;
-      const today = moment().format();
+  return (control: AbstractControl): {
+    [key: string]: any
+  } => {
+    const value = control.value;
+    const today = moment().format('YYYY-MM-DD');
 
-      if (value < today) {
-         return { 'dateInvalid' : value };
-      }
+    if (value < today) {
+      return { 'dateInvalid': value };
+    }
 
-      // if (date.diff(value) > moment.duration(1, 'years')) {
+    // if (date.diff(value) > moment.duration(1, 'years')) {
 
-      // }
-      return null;
-   };
+    // }
+    return null;
+  };
 }
--- coffeehouse-app/src/app/reservation/reservation.component.ts
+++ coffeehouse-app/src/app/reservation/reservation.component.ts
@@ -1,15 +1,15 @@
 import {
-   Component,
-   OnInit,
-   ViewChild,
-   ViewContainerRef
+  Component,
+  OnInit,
+  ViewChild,
+  ViewContainerRef
 } from '@angular/core';
 import * as firebase from 'firebase';
 import {
-   FormControl,
-   FormGroup,
-   Validators,
-   FormBuilder
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder
 } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { DataService } from '../_services/data.service';
@@ -20,82 +20,81 @@ import * as moment from 'moment';
 import { dateValidator } from '../_shared/validators/date-validator';
 
 @Component({
-   selector: 'app-reservation',
-   templateUrl: './reservation.component.html',
-   styleUrls: [
+  selector: 'app-reservation',
+  templateUrl: './reservation.component.html',
+  styleUrls: [
     '../../sass/forms.scss',
-     './reservation.component.scss'
+    './reservation.component.scss'
   ]
 })
 export class ReservationComponent {
 
-   private user: firebase.User;
+  private user: firebase.User;
 
-   reservationForm: FormGroup;
-   today = moment().format('LLLL');
+  reservationForm: FormGroup;
+  today = moment().format('LLLL');
 
-   constructor(private authService: AuthService,
-      private dataService: DataService,
-      private fb: FormBuilder,
-      public toastr: ToastsManager,
-      private vRef: ViewContainerRef) {
+  constructor(private authService: AuthService,
+    private dataService: DataService,
+    private fb: FormBuilder,
+    public toastr: ToastsManager,
+    private vRef: ViewContainerRef) {
 
-      this.toastr.setRootViewContainerRef(vRef);
+    this.toastr.setRootViewContainerRef(vRef);
 
-      this.authService.user$.subscribe(user => {
-        this.user = user;
-        this.createForm();
-      });
-   }
-
-   createForm(): void {
-      let displayName = '';
-      let email = '';
-      if (this.user !== null) {
-         displayName = this.user.displayName;
-         email = this.user.email;
-      }
-
-      this.reservationForm = this.fb.group({
-         // Use nested form groups to be able to use validated-input.component
-         requester: this.fb.group({
-            emailAddress: new FormControl(email, [
-               Validators.required,
-               emailPatternValidator()
-            ]),
-            displayName: new FormControl(displayName,
-              Validators.required)
-         }),
-         time: this.fb.group({
-           date:  new FormControl(moment().format('LLLL'), [
-             Validators.required,
-             dateValidator()
-           ]),
-           hour: new FormControl(moment().format('LLLL'))
-         }),
-         // Validation messages not needed
-         guests: new FormControl(4),
-        //  time: new FormControl(moment().format('LLLL')),
-         comments: new FormControl('')
-      });
-   }
-
-   clearForm() {
+    this.authService.user$.subscribe(user => {
+      this.user = user;
       this.createForm();
-   }
+    });
+  }
 
-   submitForm(): void {
-      this.dataService.addReservation({
-         displayName: this.reservationForm.get('displayName.displayName').value,
-         emailAddress: this.reservationForm.get('emailAddress.emailAddress').value,
-         guests: this.reservationForm.controls.guests.value,
-         date: this.reservationForm.controls.date.value,
-         comments: this.reservationForm.controls.comments.value
-      }).then((response) => {
-         this.toastr.success('Successfully saved reservation!');
-         this.clearForm();
-      }, (error) => {
-         this.toastr.error('Oh snap! Something went wrong!');
-      });
-   }
+  createForm(): void {
+    let displayName = '';
+    let email = '';
+    if (this.user !== null) {
+      displayName = this.user.displayName;
+      email = this.user.email;
+    }
+
+    this.reservationForm = this.fb.group({
+      // Use nested form groups to be able to use validated-input.component
+      requester: this.fb.group({
+        emailAddress: new FormControl(email, [
+          Validators.required,
+          emailPatternValidator()
+        ]),
+        displayName: new FormControl(displayName,
+          Validators.required)
+      }),
+      time: this.fb.group({
+        date: new FormControl(moment().format('YYYY-MM-DD'), [
+          Validators.required,
+          dateValidator()
+        ]),
+        hour: new FormControl(moment().format('HH:mm'))
+      }),
+      // Validation messages not needed
+      guests: new FormControl(4),
+      comments: new FormControl('')
+    });
+  }
+
+  clearForm() {
+    this.createForm();
+  }
+
+  submitForm(): void {
+    this.dataService.addReservation({
+      displayName: this.reservationForm.get('requester.displayName').value,
+      emailAddress: this.reservationForm.get('requester.emailAddress').value,
+      guests: this.reservationForm.controls.guests.value,
+      date: this.reservationForm.get('time.date').value,
+      comments: this.reservationForm.controls.comments.value
+    }).then((response) => {
+      this.toastr.success('Successfully saved reservation!');
+      this.clearForm();
+    }, (error) => {
+      this.toastr.error('Oh snap! Something went wrong!');
+    });
+  }
 }
